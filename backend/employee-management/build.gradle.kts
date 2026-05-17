6fe6855559fcc0a7a19e7b9c1dc77383a2bc3182
ShumpeiKawamura37 <lrivermht1611flat@icloud.com>
2026-05-18 01:12:11 +0900

    fix: jwtを修正

--- backend/employee-management/build.gradle.kts
+++ backend/employee-management/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 	java
-	id("org.springframework.boot") version "4.1.0-RC1"
+	id("org.springframework.boot") version "3.5.0"
 	id("io.spring.dependency-management") version "1.1.7"
 }
 
@@ -24,7 +24,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.ai:spring-ai-starter-model-postgresml-embedding")
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
--- backend/employee-management/src/main/java/group_assignment/employee_management/config/security/CustomUserPrincipal.java
+++ backend/employee-management/src/main/java/group_assignment/employee_management/config/security/CustomUserPrincipal.java
@@ -2,24 +2,12 @@ package group_assignment.employee_management.config.security;
 
 public class CustomUserPrincipal {
   private Long id;
-  private String email;
-  private String role;
 
-  public CustomUserPrincipal(Long id, String email, String role) {
+  public CustomUserPrincipal(Long id) {
     this.id = id;
-    this.email = email;
-    this.role = role;
   }
 
   public Long getId() {
     return id;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public String getRole() {
-    return role;
   }
 }
