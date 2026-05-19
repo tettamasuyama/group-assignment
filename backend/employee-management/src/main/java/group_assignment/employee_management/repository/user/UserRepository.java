7eec7a51740775115d3d96f1e0d98da5f783b49e
ShumpeiKawamura37 <lrivermht1611flat@icloud.com>
2026-05-19 12:17:28 +0900

    feature: repository実装

--- backend/employee-management/src/main/java/group_assignment/employee_management/repository/user/UserRepository.java
+++ backend/employee-management/src/main/java/group_assignment/employee_management/repository/user/UserRepository.java
@@ -1,9 +1,17 @@
 package group_assignment.employee_management.repository.user;
 
 import org.springframework.stereotype.Repository;
+
+import group_assignment.employee_management.entity.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public class UserRepository extends  JpaRepository<User, Long>{
+public interface UserRepository 
+  extends JpaRepository<User, Long> {
+
+  Optional<User> findByEmail(String email);
   
 }
