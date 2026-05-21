6f6b9d7c250ae0e256b6d0359d7f9c395e4f4c7a
tettamasuyama <tebetta929@gmail.com>
2026-05-21 13:47:41 +0900

    Add files via upload
    フロントエンド追加

---
+++ frontend/App.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginPage from "./page/LoginPage";
+import UserListPage from "./page/UserListPage";
+import UserEditPage from "./page/UserEditPage";
+import AdminListPage from "./page/AdminListPage";
+import AdminEditPage from "./page/AdminEditPage";
+
+/*
+* Appという名前のReactコンポーネントを作成し、
+*このファイルのメイン機能として外部公開する。
+*(ファイルパスは path="/○○○"となっており、React内では、
+* "/○○○" を使って指定のページ(element)へアクセスが可能　)
+* 例：onClick={() => navigate("/employeelist")}
+* ※ React Router は、
+*「URL に応じて、どのコンポーネント（ページ）を表示するか」
+* を定義する仕組み。
+*/
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+
+        <Route path="/userlist" element={<UserListPage />} />
+
+        <Route path="/useredit" element={<UserEditPage />} />
+
+        <Route path="/employeelist" element={<AdminListPage />} />
+
+        <Route path="/employeeedit" element={<AdminEditPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ frontend/components/decision/AdminApi.ts
@@ -0,0 +1,107 @@
+export type Employee = {
+  employeeNumber: string;
+  name: string;
+  email: string;
+  department: string;
+  role: string;
+  joinDate: string;
+  status: string;
+};
+
+/*
+ *　社員一覧を取得(全ての情報)
+ */
+
+export async function getEmployeeList(): Promise<Employee[]> {
+  //ブラウザ保存領域から token を取得
+  const token = localStorage.getItem("token");
+
+  const response = await fetch("/api/employees", {
+    method: "GET",
+
+    headers: {
+      "Content-Type": "application/json",
+      //私はログイン済みユーザーの証明
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("社員一覧取得失敗");
+  }
+
+  return await response.json();
+}
+
+/*
+ *　社員情報を新規登録or更新
+ */
+
+export async function employeeRegistration(employeeNumber: Employee) {
+  const token = localStorage.getItem("token");
+
+  const response = await fetch(`/api/employees/${employeeNumber}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(employeeNumber),
+  });
+
+  if (!response.ok) {
+    throw new Error("登録・更新失敗");
+  }
+
+  return await response.json();
+}
+
+/*
+ *　社員情報を削除
+ */
+
+export async function employeeDelete(employeeNumber: string) {
+  const token = localStorage.getItem("token");
+
+  const response = await fetch(`/api/employees/${employeeNumber}`, {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error("削除失敗");
+  }
+
+  return await response.json();
+}
+
+/*
+ *　CSVで一括登録・更新
+ */
+
+export async function csvUpload(file: File) {
+  const token = localStorage.getItem("token");
+
+  const formData = new FormData();
+
+  formData.append("file", file);
+
+  const response = await fetch("/api/employees/import", {
+    method: "POST",
+
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+
+    body: formData,
+  });
+
+  if (!response.ok) {
+    throw new Error("CSVアップロード失敗");
+  }
+
+  return await response.json();
+}
+
