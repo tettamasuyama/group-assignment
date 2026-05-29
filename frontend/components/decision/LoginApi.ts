d2bff6059e1d1e32b6e16249212b18c930509940
tettamasuyama <tebetta929@gmail.com>
2026-05-29 17:53:28 +0900

    Add files via upload

--- frontend/components/decision/LoginApi.ts
+++ frontend/components/decision/LoginApi.ts
@@ -1,43 +1,52 @@
 import { mailDecision, passwordDecision } from "../decision/InputDecision";
 
 /*
-* ログインを行い、JWTを保存
-*/
+ * ログインを行い、JWTを保存
+ */
 
 export async function loginApi(email: string, password: string) {
   try {
-
-    if(email===""){
-    alert("メールアドレスが未入力です");
-    return
+    if (email === "") {
+      alert("メールアドレスが未入力です");
+      return;
     }
 
-    if(password===""){
-    alert("パスワードが未入力です");
-    return
+    if (password === "") {
+      alert("パスワードが未入力です");
+      return;
     }
-
 
     if (mailDecision(email)) {
       if (passwordDecision(password)) {
-        const response = await fetch("/api/auth/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
+        const response = await fetch(
+          // "/api/auth/login"
+          "http://localhost:8080/api/auth/login",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email,
+              password,
+            }),
           },
-          body: JSON.stringify({
-            email,
-            password,
-          }),
-        });
+        );
 
         const data = await response.json();
 
-        // token保存
-        localStorage.setItem("token", data.token);
+        // console.log(data);
+        // console.log("role =", data.role);
 
         if (response.ok) {
-          window.location.href = "/UserListPage";
+          // token保存
+            localStorage.setItem("token", data.token);
+
+          if (data.role === "ADMIN") {
+            window.location.href = "/AdminListPage";
+          } else {
+            window.location.href = "/UserListPage";
+          }
         } else {
           alert("アクセスに失敗しました");
         }
